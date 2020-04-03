1b29dc64c0a234c3dfdee56ff1895196accd0491
unknown <2721025075@qq.com>
2020-04-03 17:34:45 +0800

    zuoye

--- go_project/课后作业/panduanzifuchuan/main.go
+++ go_project/课后作业/panduanzifuchuan/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	// "unicode"
+	"strings"
+)
+
 func main() {
 	// 	s1 := "存卡啊啊 额;啊123shaha ana"
 
@@ -15,14 +21,21 @@ func main() {
 	//判断字符串的有多少个
 
 	s2 := "how do you do"
+	// s3 := strings.Split(s2 , " ")
+	s3 := strings.Split(s2, " ")
+	//     s2 := "how do you do"
+	// s3 := strings.Split(s2 , " ")
 
 	m1 := make(map[string]int, 10)
-	for _, w := range s2 {
+	for _, w := range s3 {
+		// fmt.Println(string(w1))
 		if _, ok := m1[w]; !ok {
 			m1[w] = 1
 		} else {
 			m1[w]++
 		}
+		fmt.Println(w)
 	}
+	
 
 }
