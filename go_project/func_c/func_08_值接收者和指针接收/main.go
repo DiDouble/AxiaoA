d66810f9c1af1083aec11f177bdae9dbfeaf2686
DoubleTu <wdy5627919@gmail.com>
2020-04-17 10:32:03 +0800

    11

--- go_project/func_c/func_08_值接收者和指针接收/main.go
+++ go_project/func_c/func_08_值接收者和指针接收/main.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+type people struct {
+	name string 
+	age int 
+}
+
+//方法一：
+var p persion  //声明一个person类型的变量p
+p.name = "11"
+p.age = "18"
+fmt.Println(p)
+//方法二
+persion {
+	name: "22",
+	age: 15,
+}
+
 // 给自定义类型方法
 // 不能给别的包里面的类型添加方法，只能给自己包添加
 type myInt int
@@ -15,6 +31,8 @@ func (m myInt) myintd() {
 
 func main() {
 	m := myInt(100)
+	var x  int 100
+	var x = 100    //以上三个是一样的。
 	m.myintd()
 
 }
--- go_project/课后作业/学生管理系统/main.go
+++ go_project/课后作业/学生管理系统/main.go
@@ -1,42 +1,41 @@
+
 package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func selectStudent() {
-
-}
-func addStudent() {
-
-}
-func deleteStudent() {
-
-}
-
-func main() {
-	fmt.Println("欢迎来到学生管理系统")
-	fmt.Println(`
-		1. 查询学生
-		2. 增加学生
+func main()  {
+	//打印菜单
+	fmt.Println("欢迎光临学生管理系统")
+	fmt.Println(
+		`
+		1. 查看所有学生
+		2. 新增学生
 		3. 删除学生
 		4. 退出
-		`)
-	fmt.Print("你要干啥:")
-	var casina int
-	fmt.Scanln(&casina)
-	fmt.Printf("你输入的是%d", casina)
-	switch casina {
+		`
+	)
+	fmt.Print("请输入你要干啥:")
+	//等待用户做选择
+	var choice int
+	fmt.Scanln(&choice)
+	fmt.Printf("你选择的选项是!\n",choice)
+
+	//3. 执行对应的函数
+	switch choice {
 	case 1:
-		selectStudent()
+		showAllStudent()
 	case 2:
 		addStudent()
 	case 3:
 		deleteStudent()
 	case 4:
-		os.Exit(1)
+		os.Exit(1) //退出,返回状态码是1
 	default:
 		fmt.Println("滚~~")
+
 	}
+	
 }
+
