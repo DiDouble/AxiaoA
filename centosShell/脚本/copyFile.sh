d2634fb1d631e44439cedb84b73ca9c51dbcdf9f
Oldyuan <5457960+oldyuan@user.noreply.gitee.com>
2020-05-07 10:56:17 +0800

    增加判断

--- centosShell/脚本/copyFile.sh
+++ centosShell/脚本/copyFile.sh
@@ -2,6 +2,14 @@
 
 # Author: Oldyuan
 
+searchFile() {
+    fileName=`/usr/bin/find /home/gitlab/backups/ -name "*$(date +%Y_%m_%d)*"`
+    if [ -z $fileName ];then
+        echo "没有生成"
+    else
+        copyFile
+    fi
+}
 
 copyFile() {
     dateTime=`date +"%Y_%m_%d"`
@@ -9,14 +17,14 @@ copyFile() {
     echo $fileName
     scp $fileName root@192.168.0.187:/data/gitlabback
     if [ $? -ne 0 ]; then
-        echo "失败的"
+        echo "失败的，用于邮件发送失败吧"
     else
         exit
     fi
 }
 
 main() {
-    copyFile
+    searchFile
 }
 
 main
